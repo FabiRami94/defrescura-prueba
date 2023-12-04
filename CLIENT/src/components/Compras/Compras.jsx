6339ce3fbb71cf829627a1f73602f2b0c9a58669
FabiRami94 <fabioerv_1994@hotmail.com>
2023-12-04 01:37:41 -0500

    backend

--- CLIENT/src/components/Compras/Compras.jsx
+++ CLIENT/src/components/Compras/Compras.jsx
@@ -1,10 +1,10 @@
-
 import styles from './Compras.module.css';
-import React from 'react';
-import {SuccessDeleteCart} from '../../assets/modals/modals.js';
+import React, { useState } from 'react';
+import { SuccessDeleteCart } from '../../assets/modals/modals.js';
 
-const Compras = ({addToCart, cart, setCart}) => {
-  
+const Compras = ({ addToCart, cart, setCart }) => {
+  const [confirmation, setConfirmation] = useState(false);
+
   const removeFromCart = (productId) => {
     const updatedCart = cart.filter(item => item.id !== productId);
     setCart(updatedCart);
@@ -21,49 +21,89 @@ const Compras = ({addToCart, cart, setCart}) => {
     return cart.reduce((total, item) => total + item.price * item.quantity, 0);
   };
 
+  const handleConfirmPurchase = () => {
+    setConfirmation(prevConfirmation => !prevConfirmation);
+  };
+
+  const renderConfirmationTable = () => {
+    return (
+      <div>
+        <h2>Detalle de la Compra</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Precio</th>
+              <th>Cantidad</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map(item => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>{item.quantity}</td>
+                <td>{item.price * item.quantity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <p>Total: {calculateTotal()}</p>
+        <button className={styles.GeneralButton} onClick={handleConfirmPurchase }>Atrás</button>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <h1 style={{display: 'flex', flexDirection: 'column', textAlign: 'center'}}>CARRITO DE COMPRAS</h1>
+      <h1 style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>CARRITO DE COMPRAS</h1>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         {cart.length === 0 ? (
           <p style={{ textAlign: 'center' }}>Debes agregar algún producto para poder comprar.</p>
         ) : (
           <>
-            <table>
-              <thead>
-                <tr>
-                  <th>Nombre</th>
-                  <th>Precio</th>
-                  <th>Cantidad</th>
-                  <th>Total</th>
-                  <th>Acciones</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cart.map(item => (
-                  <tr key={item.id}>
-                    <td>{item.name}</td>
-                    <td>{item.price}</td>
-                    <td>
-                      <button className={styles.GeneralButton} onClick={() => decreaseQuantity(item.id)}>-</button>
-                      {item.quantity}
-                      <button className={styles.GeneralButton} onClick={() => addToCart(item)}>+</button>
-                    </td>
-                    <td>{item.price * item.quantity}</td>
-                    <td>
-                      <button className={styles.GeneralButton} onClick={() => {removeFromCart(item.id), SuccessDeleteCart()}}>Quitar</button>
-                    </td>
+            {confirmation ? (
+              renderConfirmationTable()
+            ) : (
+              <table>
+                <thead>
+                  <tr>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                    <th>Cantidad</th>
+                    <th>Total</th>
+                    <th>Acciones</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {cart.map(item => (
+                    <tr key={item.id}>
+                      <td>{item.name}</td>
+                      <td>{item.price}</td>
+                      <td>
+                        <button className={styles.GeneralButton} onClick={() => decreaseQuantity(item.id)}>-</button>
+                        {item.quantity}
+                        <button className={styles.GeneralButton} onClick={() => addToCart(item)}>+</button>
+                      </td>
+                      <td>{item.price * item.quantity}</td>
+                      <td>
+                        <button className={styles.GeneralButton} onClick={() => { removeFromCart(item.id); SuccessDeleteCart(); }}>Quitar</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
           </>
         )}
       </div>
+      {!confirmation && (
         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
           <p>Total: {calculateTotal()}</p>
-          <button className={styles.GeneralButton}>Confirmar Compra</button>
+          <button className={styles.GeneralButton} onClick={handleConfirmPurchase}>Confirmar Compra</button>
         </div>
+      )}
     </div>
   );
 };
--- SERVER/server.js
+++ SERVER/server.js
@@ -4,7 +4,7 @@ const { conn } = require('./src/db.js');
 require('dotenv').config();
 const {PORT} = process.env;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
     server.listen(PORT, () => {
       console.log(`Server levantado en el Puerto ${PORT}`); // eslint-disable-line no-console
     });
